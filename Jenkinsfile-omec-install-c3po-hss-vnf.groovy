8409935a61426add59166c71b7ae3f5df5f0f883
micpez <michele.pezzutti@hcl.com>
2019-04-30 15:18:47 +0200

    Move install logic to scripts in .ci dir of c3po repo
    Move install step logic from groovy files to bash scripts in .ci directory
    in c3po repo.

--- Jenkinsfile-omec-install-c3po-hss-vnf.groovy
+++ Jenkinsfile-omec-install-c3po-hss-vnf.groovy
@@ -17,37 +17,11 @@ node("${params.executorNode}") {
   def ghRepository = 'omec-project/c3po'
 
   def install_path = '/home/jenkins'
-  def basedir_dp1 = '/home/jenkins'  //for keys installation
-
-  def base_path = '/var/log/cicd'
-  def base_folder = 'install'
-  def base_log_dir = "${base_path}/${base_folder}"
-
-  def hss_app = 'hss'
-
-  def hss_dir = "${base_log_dir}/${hss_app}"
-
-  def action_inst = '_install'
-  def action_make = '_make'
-
-  def stdout_ext = '.stdout.log'
-  def stderr_ext = '.stderr.log'
-
-  def hss_install_stdout_log = "${hss_dir}/" + "hss" + "${action_inst}${stdout_ext}"
-  def hss_install_stderr_log = "${hss_dir}/" + "hss" + "${action_inst}${stderr_ext}"
-  def hsssec_stdout_log = "${hss_dir}/" + "hsssec" + "${action_make}${stdout_ext}"
-  def hsssec_stderr_log = "${hss_dir}/" + "hsssec" + "${action_make}${stderr_ext}"
-  def hss_stdout_log = "${hss_dir}/" + "hss" + "${action_make}${stdout_ext}"
-  def hss_stderr_log = "${hss_dir}/" + "hss" + "${action_make}${stderr_ext}"
 
   timeout (60) {
     try {
 
       echo "${params}"
-
-      // to check and update SGX dealer / DP keys
-      def mrenclave = ''
-      def mrsigner = ''
 
       stage("VMs check") {
         timeout(1) {
@@ -56,71 +30,36 @@ node("${params.executorNode}") {
             virsh list --all
             """
             echo "Running VMs: ${running_vms}"
-            return true
-          }
-        }
-        timeout(1) {
-          waitUntil {
+
             running_vms = sh returnStdout: true, script: """
             virsh list --all | grep c3po-hss1 | grep -i running | wc -l
             """
             return running_vms.toInteger() == 1
           }
         }
-        // Clean all logs
-        sh returnStdout: true, script: """
-        ssh c3po-hss1 '
-            if [ ! -d "${hss_dir}" ]; then mkdir -p ${hss_dir}; fi
-            rm -fr ${hss_dir}/*
-            '
-        """
       }
 
-      stage("clone c3po for HSS") {
-        timeout(20) {
-          sh returnStdout: true, script: """ssh c3po-hss1 'if pgrep -f [h]ss; then pkill -f [h]ss; fi'"""
-          waitUntil {
-            hss_c3po_clone_output = sh returnStdout: true, script: """
-            ssh c3po-hss1 '
-                cd ${install_path}
-                rm -rf c3po
-                git clone https://github.com/omec-project/c3po.git || exit 1
-                cd c3po
+      stage("Install HSS") {
+        timeout(10) {
+          sh script: """
+          ssh c3po-hss1 '
+              if [[ ! -d "${install_path}" ]]; then mkdir -p ${install_path}; fi
 
-                if [ ${params.ghprbGhRepository} = ${ghRepository} ]; then
-                    git fetch origin pull/${params.ghprbPullId}/head:jenkins_test || exit 1
-                    git rebase master jenkins_test || exit 1
-                    git log -1
-                fi
-                '
-            """
-            echo "${hss_c3po_clone_output}"
-            return true
-          }
+              cd ${install_path}
+              rm -rf c3po
+              git clone https://github.com/omec-project/c3po.git || exit 1
+              cd c3po
 
-          waitUntil {
-            hss_dealer_in_output = sh returnStdout: true, script: """
-            ssh c3po-hss1 'cd ${install_path}/c3po && ./install.sh < ${install_path}/c3po/.ci/install/hss-auto-install.txt 1>${hss_install_stdout_log} 2>${hss_install_stderr_log}'
-            """
-            echo "${hss_dealer_in_output}"
-            return true
-          }
-          sh returnStdout: true, script: """
-          ssh c3po-hss1 'cd ${install_path}/c3po/util && make clean && make 1>${hsssec_stdout_log} 2>${hsssec_stderr_log}'
-          ssh c3po-hss1 'cd ${install_path}/c3po/hsssec && make clean && make 1>>${hsssec_stdout_log} 2>>${hsssec_stderr_log}'
+              if [ ${params.ghprbGhRepository} = ${ghRepository} ]; then
+                  git fetch origin pull/${params.ghprbPullId}/head:jenkins_test || exit 1
+                  git rebase master jenkins_test || exit 1
+                  git log -1
+              fi
+              '
           """
 
-          waitUntil {
-            hss_hss_output = sh returnStdout: true, script: """
-            ssh c3po-hss1 'cp -f ${install_path}/c3po/.ci/config/hss.json ${install_path}/c3po/hss/conf/hss.json'
-
-            ssh c3po-hss1 'cd ${install_path}/c3po/hss/conf && ../bin/make_certs.sh hss openair4G.eur'
-            """
-            echo "${hss_hss_output}"
-            return true
-          }
-          sh returnStdout: true, script: """
-          ssh c3po-hss1 'cd ${install_path}/c3po/hss && make clean && make 1>${hss_stdout_log} 2>${hss_stderr_log}'
+          sh script: """
+          ssh c3po-hss1 ${install_path}/c3po/.ci/install/hss/install_hss.sh ${install_path}
           """
         }
       }
@@ -133,14 +72,19 @@ node("${params.executorNode}") {
       rm -fr *
       """
 
+      hss_dir = sh returnStdout: true, script: """
+      ssh c3po-hss1 '${install_path}/c3po/.ci/common/get_log_dir.sh ${install_path} hss'
+      """
+      hss_dir = hss_dir.replace('\n', '')
+
       try {
         sh returnStdout: true, script: """
         scp -r c3po-hss1:${hss_dir} .
         """
       } catch (err) {}
 
-      archiveArtifacts artifacts: "**/*${stdout_ext}", allowEmptyArchive: true
-      archiveArtifacts artifacts: "**/*${stderr_ext}", allowEmptyArchive: true
+      archiveArtifacts artifacts: "**/*", allowEmptyArchive: true
+      archiveArtifacts artifacts: "**/*", allowEmptyArchive: true
 
     }
     echo "RESULT: ${currentBuild.result}"
--- Jenkinsfile-omec-install-c3po-sgx-vnf.groovy
+++ Jenkinsfile-omec-install-c3po-sgx-vnf.groovy
@@ -19,45 +19,10 @@ node("${params.executorNode}") {
   def install_path = '/home/jenkins'
   def basedir_dp1 = '/home/jenkins'  //for keys installation
 
-  def base_path = '/var/log/cicd'
-  def base_folder = 'install'
-  def base_log_dir = "${base_path}/${base_folder}"
-
-  def sgx_app = 'sgx'
-
-  def sgx_dir = "${base_log_dir}/${sgx_app}"
-
-  def action_inst = '_install'
-  def action_make = '_make'
-
-  def stdout_ext = '.stdout.log'
-  def stderr_ext = '.stderr.log'
-
-  def dealer_install_stdout_log = "${sgx_dir}/" + "dealer" + "${action_inst}${stdout_ext}"
-  def dealer_install_stderr_log = "${sgx_dir}/" + "dealer" + "${action_inst}${stderr_ext}"
-  def dealer_stdout_log = "${sgx_dir}/" + "dealer" + "${action_make}${stdout_ext}"
-  def dealer_stderr_log = "${sgx_dir}/" + "dealer" + "${action_make}${stderr_ext}"
-  def kms_install_stdout_log = "${sgx_dir}/" + "kms" + "${action_inst}${stdout_ext}"
-  def kms_install_stderr_log = "${sgx_dir}/" + "kms" + "${action_inst}${stderr_ext}"
-  def kms_stdout_log = "${sgx_dir}/" + "kms" + "${action_make}${stdout_ext}"
-  def kms_stderr_log = "${sgx_dir}/" + "kms" + "${action_make}${stderr_ext}"
-  def router_install_stdout_log = "${sgx_dir}/" + "router" + "${action_inst}${stdout_ext}"
-  def router_install_stderr_log = "${sgx_dir}/" + "router" + "${action_inst}${stderr_ext}"
-  def util_stdout_log = "${sgx_dir}/" + "util" + "${action_make}${stdout_ext}"
-  def util_stderr_log = "${sgx_dir}/" + "util" + "${action_make}${stderr_ext}"
-  def ctf_stdout_log = "${sgx_dir}/" + "ctf" + "${action_make}${stdout_ext}"
-  def ctf_stderr_log = "${sgx_dir}/" + "ctf" + "${action_make}${stderr_ext}"
-  def cdf_stdout_log = "${sgx_dir}/" + "cdf" + "${action_make}${stdout_ext}"
-  def cdf_stderr_log = "${sgx_dir}/" + "cdf" + "${action_make}${stderr_ext}"
-
   timeout (60) {
     try {
 
       echo "${params}"
-
-      // to check and update SGX dealer / DP keys
-      def mrenclave = ''
-      def mrsigner = ''
 
       stage("VMs check") {
         timeout(1) {
@@ -66,194 +31,38 @@ node("${params.executorNode}") {
             virsh list --all
             """
             echo "Running VMs: ${running_vms}"
-            return true
+
+            running_vms = sh returnStdout: true, script: """
+            virsh list --all | grep ngic-dp1 | grep -i running | wc -l
+            """
+            return running_vms.toInteger() == 1
           }
         }
-        // Clean all logs
-        sh returnStdout: true, script: """
-        ssh sgx-kms-cdr '
-            if [ ! -d "${sgx_dir}" ]; then mkdir -p ${sgx_dir}; fi
-            rm -fr ${sgx_dir}/*
-            '
-        """
       }
 
-      stage("clone c3po for SGX") {
-        timeout(20) {
+
+      stage("Install SGX-Dealer") {
+        timeout(10) {
           sh returnStdout: true, script: """
           ssh sgx-kms-cdr '
-              if pgrep ctf; then pkill ctf; fi
-              if pgrep cdf; then pkill cdf; fi
-              if pgrep kms; then pkill kms; fi
-              if pgrep -x dealer; then pkill -x dealer; fi
-              if pgrep -x dealer-out; then pkill -x dealer-out; fi
+              if [[ ! -d "${install_path}" ]]; then mkdir -p ${install_path}; fi
+
+              cd ${install_path}
+              rm -rf c3po
+              git clone https://github.com/omec-project/c3po.git || exit 1
+              cd c3po
+
+              if [ ${params.ghprbGhRepository} = ${ghRepository} ]; then
+                  git fetch origin pull/${params.ghprbPullId}/head:jenkins_test || exit 1
+                  git rebase master jenkins_test || exit 1
+                  git log -1
+              fi
               '
           """
-          waitUntil {
-            sgx_c3po_clone_output = sh returnStdout: true, script: """
-            ssh sgx-kms-cdr '
-                cd ${install_path}
-                rm -rf c3po
-                git clone https://github.com/omec-project/c3po.git || exit 1
-                cd c3po
 
-                if [ ${params.ghprbGhRepository} = ${ghRepository} ]; then
-                    git fetch origin pull/${params.ghprbPullId}/head:jenkins_test || exit 1
-                    git rebase master jenkins_test || exit 1
-                    git log -1
-                fi
-                '
-            """
-            echo "${sgx_c3po_clone_output}"
-            return true
-          }
-
-          waitUntil {
-            c3po_dealer_output = sh returnStdout: true, script: """
-            ssh sgx-kms-cdr 'cd ${install_path}/c3po/sgxcdr/dealer && ./install.sh < ${install_path}/c3po/.ci/install/sgx-auto-install.txt 1>${dealer_install_stdout_log} 2>${dealer_install_stderr_log}'
-            ssh sgx-kms-cdr 'cp -f ${install_path}/c3po/.ci/config/dealer-in.json ${install_path}/c3po/sgxcdr/dealer/conf/dealer.json'
-            """
-            echo "${c3po_dealer_output}"
-            return true
-          }
-          sh returnStdout: true, script: """
-          ssh sgx-kms-cdr 'cd ${install_path}/c3po/sgxcdr/dealer && make clean && make SGX_MODE=HW SGX_DEBUG=1 1>${dealer_stdout_log} 2>${dealer_stderr_log}'
+          sh script: """
+          ssh sgx-kms-cdr ${install_path}/c3po/.ci/install/sgx/install_sgx.sh ${install_path}
           """
-        }
-
-        // update ca_bundle.h with MRENCLAVE / MRSIGNER
-        timeout(1) {
-          waitUntil {
-            keys = sh returnStdout: true, script: """
-            ssh sgx-kms-cdr '
-                cd ${install_path}/c3po/sgxcdr/dealer
-                output=(\$(./dealer -j conf/dealer.json -x | sed -n \"s/MR.* ://p\"))
-
-                MRENCLAVE=\${output[0]}
-                MRSIGNER=\${output[1]}
-
-                echo \$MRENCLAVE
-                echo \$MRSIGNER
-               '
-            """
-            //echo "${keys}"
-            (mrenclave, mrsigner) = keys.trim().split("\n")
-            echo "${mrenclave}"
-            echo "${mrsigner}"
-            return true
-          }
-
-          waitUntil {
-            ca_bundle_output = sh returnStdout: true, script: """
-            ssh sgx-kms-cdr '
-                cd ${install_path}/c3po/sgxcdr/kms/Enclave
-                sed -i \"0,/\\\"\\\",/{s/\\\"\\\",/\\\"${mrenclave}\\\",/}\" ca_bundle.h
-                sed -i \"0,/\\\"\\\",/{s/\\\"\\\",/\\\"${mrsigner}\\\",/}\" ca_bundle.h
-               '
-            """
-            echo "${ca_bundle_output}"
-            return true
-          }
-        }
-
-        timeout(5) {
-          waitUntil {
-            c3po_kms_output = sh returnStdout: true, script: """
-            ssh sgx-kms-cdr 'cd ${install_path}/c3po/sgxcdr/kms && ./install.sh < ${install_path}/c3po/.ci/install/sgx-auto-install.txt 1>${kms_install_stdout_log} 2>${kms_install_stderr_log}'
-            ssh sgx-kms-cdr 'cp -f ${install_path}/c3po/.ci/config/kms.json ${install_path}/c3po/sgxcdr/kms/conf/kms.json'
-            """
-            echo "${c3po_kms_output}"
-            return true
-          }
-          sh returnStdout: true, script: """
-          ssh sgx-kms-cdr 'cd ${install_path}/c3po/sgxcdr/kms && make clean && make SGX_MODE=HW SGX_DEBUG=1 1>${kms_stdout_log} 2>${kms_stderr_log}'
-          """
-        }
-
-        timeout(5) {
-          waitUntil {
-            c3po_dealer_out_output = sh returnStdout: true, script: """
-            ssh sgx-kms-cdr 'cd ${install_path}/c3po/sgxcdr && cp -R dealer dealer-out'
-
-            ssh sgx-kms-cdr 'cd ${install_path}/c3po/sgxcdr/dealer-out && mv dealer dealer-out'
-            ssh sgx-kms-cdr 'cd ${install_path}/c3po/sgxcdr/dealer-out && mkdir cdr'
-            ssh sgx-kms-cdr 'cp -f ${install_path}/c3po/.ci/config/dealer-out.json ${install_path}/c3po/sgxcdr/dealer-out/conf/dealer.json'
-            """
-            echo "${c3po_dealer_out_output}"
-            return true
-          }
-        }
-
-        timeout(2) {
-          waitUntil {
-            c3po_router_output = sh returnStdout: true, script: """
-            ssh sgx-kms-cdr 'cd ${install_path}/c3po/sgxcdr/router && ./install.sh 1>${router_install_stdout_log} 2>${router_install_stderr_log}'
-            """
-            echo "${c3po_router_output}"
-            return true
-          }
-        }
-
-        timeout(5) {
-          waitUntil {
-            c3po_ctf_output = sh returnStdout: true, script: """
-            ssh sgx-kms-cdr 'cp -f ${install_path}/c3po/.ci/config/ctf.json ${install_path}/c3po/ctf/conf/ctf.json'
-            """
-            echo "${c3po_ctf_output}"
-            return true
-          }
-          sh returnStdout: true, script: """
-          ssh sgx-kms-cdr 'cd ${install_path}/c3po/util && make clean && make 1>${util_stdout_log} 2>${util_stderr_log}'
-          ssh sgx-kms-cdr 'cd ${install_path}/c3po/ctf && mkdir logs && make clean && make 1>${ctf_stdout_log} 2>${ctf_stderr_log}'
-          """
-          waitUntil {
-            c3po_ctf_conf_output = sh returnStdout: true, script: """
-            ssh sgx-kms-cdr 'cd ${install_path}/c3po/ctf/conf && ../bin/make_certs.sh ctf test3gpp.net'
-            """
-            echo "${c3po_ctf_conf_output}"
-            return true
-          }
-        }
-
-        timeout(5) {
-          waitUntil {
-            c3po_cdf_output = sh returnStdout: true, script: """
-            ssh sgx-kms-cdr 'cp -f ${install_path}/c3po/.ci/config/cdf.conf ${install_path}/c3po/cdf/conf/cdf.conf'
-            """
-            echo "${c3po_cdf_output}"
-            return true
-          }
-          sh returnStdout: true, script: """
-          ssh sgx-kms-cdr 'cd ${install_path}/c3po/cdf && make clean && make 1>${cdf_stdout_log} 2>${cdf_stderr_log}'
-          """
-          waitUntil {
-            c3po_cdf_conf_output = sh returnStdout: true, script: """
-            ssh sgx-kms-cdr 'cd ${install_path}/c3po/cdf/conf && ../bin/make_certs.sh sgx-kms-cdr test3gpp.net'
-            """
-            echo "${c3po_cdf_conf_output}"
-            return true
-          }
-        }
-      }
-
-      // update MRENCLAVE / MRSIGNER in ngic-rtc/config/interface.cfg
-      timeout(1) {
-        waitUntil {
-          ngic_rtc_output = sh returnStdout: true, script: """
-          ssh ngic-dp1 '
-              cd ${basedir_dp1}/ngic-rtc/config
-
-              MRENCLAVE=${mrenclave}
-              MRSIGNER=${mrsigner}
-
-              sed -i \"s/dealer_in_mrenclave *= *.*/dealer_in_mrenclave = \${MRENCLAVE}/\" interface.cfg
-              sed -i \"s/dealer_in_mrsigner *= *.*/dealer_in_mrsigner = \${MRSIGNER}/\" interface.cfg
-
-              grep -P \"dealer_in_mrenclave|dealer_in_mrsigner\" interface.cfg
-             '
-          """
-          echo "${ngic_rtc_output}"
-          return true
         }
       }
 
@@ -266,14 +75,19 @@ node("${params.executorNode}") {
       rm -fr *
       """
 
+      sgx_dir = sh returnStdout: true, script: """
+      ssh sgx-kms-cdr '${install_path}/c3po/.ci/common/get_log_dir.sh ${install_path} sgx'
+      """
+      sgx_dir = sgx_dir.replace('\n', '')
+
       try {
         sh returnStdout: true, script: """
         scp -r sgx-kms-cdr:${sgx_dir} .
         """
       } catch (err) {}
 
-      archiveArtifacts artifacts: "**/*${stdout_ext}", allowEmptyArchive: true
-      archiveArtifacts artifacts: "**/*${stderr_ext}", allowEmptyArchive: true
+      archiveArtifacts artifacts: "**/*", allowEmptyArchive: true
+      archiveArtifacts artifacts: "**/*", allowEmptyArchive: true
 
     }
     echo "RESULT: ${currentBuild.result}"

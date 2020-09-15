7e9009a270ff0304d0c71f52d78059746590929a
Jeremy Ronquillo <jeremyr@opennetworking.org>
2020-09-15 09:28:23 -0700

    Fix plot to support up to 15 builds

--- metrics/scaling/scaling.R
+++ metrics/scaling/scaling.R
@@ -247,14 +247,14 @@ print( "Generating line plot." )
 lineGraphFormat <- geom_line( size = 1.1 )
 pointFormat <- geom_point( size = 4 )
 pointLabel <- geom_text_repel(aes(label=display_quantity),
-                              size = 9)
+                              size = 7)
 
 result <- fundamentalGraphDataAttach +
            lineGraphFormat +
            pointFormat +
            pointLabel
 
-imageWidth <- 15
+imageWidth <- 25
 imageHeight <- 10
 imageDPI <- 200
 
